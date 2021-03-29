a8814bd26b1d04eda63c7941b0312a2932024401
Hasmik Varosyan <hasvart@gmail.com>
2021-03-29 15:44:55 -0500

    added nextSlide function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let timer;
+let deleteFirstPhotoDelay;
+
+
 //fetching data from a server
 async function start() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
@@ -35,15 +39,34 @@ function createSlideshow(images) {
   clearInterval(timer)
   clearTimeout(deleteFirstPhotoDelay);
 
+  if(images.length > 1 ) {
+
   document.getElementById("slideshow").innerHTML =`
  <div class="slide" style="background-image: url('${images[0]}')"> </div>
  <div class="slide" style="background-image: url('${images[1]}')"> </div>
 `
 currentPosition += 2;
-setInterval(nextSlide, 3000)
-
-function nextSlide (){
+if(images.length == 2) currentPosition = 0
+timer = setInterval(nextSlide, 3000)
+  } else {
+    document.getElementById("slideshow").innerHTML =`
+ <div class="slide" style="background-image: url('${images[0]}')"> </div>
+ <div class="slide"></div>
+`
+  }
   
+function nextSlide (){
+  document.getElementById("slideshow").insertAdjacentHTML("beforeend",` <div class="slide" style="background-image: url('${images[currentPosition]}')"> </div>`)
+  deleteFirstPhotoDelay = setTimeout(function () {
+    document.querySelector(".slide").remove()
+  }, 1000)
+if(currentPosition + 1 >= images.length) {
+ currentPosition = 0;
+
+}else {
+  currentPosition ++;
+
+    } 
   }
 }
  
